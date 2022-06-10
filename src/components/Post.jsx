671f2f2abdc8aac047c6c723a6a330c202bcc494
Richard Clarke <freshnewrich@gmail.com>
2022-06-10 16:51:58 -0700

    create Post.jsx

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,18 +1,19 @@
 //import Link for use in hitting Show route
+import Link from "react-router-dom"
 
 //this is the "card"
 //this Component displays within List
 //is rendered by the map of posts array in List page
 
-export default function Post(props) {
-    //props are going to be img title id
-
-    return <div className="post">
-
-        {/* //displays image, displays title, Link is to Show route with id */}
-        {/* //entire thing is basically a Link to show route */}
-
-    </div>
-
-
+export default function Post({ key, id, title, img }) {
+    return (
+        <Link key={key} className="post" to={`/post/${id}`}>
+            <div key={key} className="post">
+                {/* //entire thing is basically a Link to show route */}
+                {/* //displays image, displays title, Link is to Show route with id */}
+                <img src={img} alt="image" className="post-image" />
+                <div className="post-title">{title}</div>
+            </div>
+        </Link>
+    )
 }
