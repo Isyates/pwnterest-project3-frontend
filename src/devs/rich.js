b409245801fdc42af3bd4bdec82a7e32569d55cf
Richard Clarke <freshnewrich@gmail.com>
2022-06-10 17:01:18 -0700

    rich.js dev file 👽

--- src/devs/rich.js
+++ src/devs/rich.js
@@ -1,7 +1,7 @@
 export default {
-    name: "Dev Name",
-    img: "img.jpeg", //from public folder, we can dump images there
-    github: "https://????.github.com",
-    blurb: "Here is a short bio, we need to decide what length the bio will be."
+    name: "Rich Clarke",
+    img: "https://raw.githubusercontent.com/richclarke0/richclarke0.github.io/main/img/me-sm.jpg", //from public folder, we can dump images there
+    github: "https://richclarke0.github.com",
+    blurb: "I wish I was a little bit taller, I wish I was a baller, I wish I had a rabbit in a hat with a bat and a 6-4' Impala"
     //perhaps add something else?
 }
