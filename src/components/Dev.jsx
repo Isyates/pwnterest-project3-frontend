4a7c25cd17e84e192ced62ec7760aa58c07dc15b
Richard Clarke <freshnewrich@gmail.com>
2022-06-11 15:40:45 -0700

    updates and styling for About, Dev

--- src/components/Dev.jsx
+++ src/components/Dev.jsx
@@ -1,11 +1,43 @@
-//no imports
+//styled components
+import styled from "styled-components"
 
-export default function Dev({dev}) {
-        //receives a bunch of props from each iteration of the map of the dev array
-        const {name, img, github, blurb} = dev
-    return <div className="dev">
-            
-        {/* //renders those props */}
+const DevContainer = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 4fr;
+    background-color: grey;
+    border-radius: 4em;
+    height: 8em;
+    width: 60%;
+`
+const DevImg = styled.img`
+    object-fit: cover;
+    width:7em;
+    height:7em;
+    border-radius: 4em;
+    margin: .5em  
+`
+const DevStats = styled.div`
+    display: flex;
+    flex-flow: column nowrap;
+    justify-content: center;
+    color: white;
+    font-size: .8em;
+    width: 90%; 
+`
 
-    </div>
+export default function Dev({ dev, key }) {
+
+    //receives a bunch of props from each iteration of the map of the dev array
+    const { name, img, github, blurb } = dev //destructuring
+
+    return (
+        <DevContainer key={key}>
+                    <DevImg src={img} alt={name}/>
+                    <DevStats>
+                        <h5>{name}</h5>
+                        <a href={github}>{github.slice(github.indexOf("/")+2)}</a>
+                        <p>{blurb}</p>
+                    </DevStats>
+        </DevContainer>
+    )
 }
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -13,6 +13,9 @@
 //Dev component
 import Dev from "../components/Dev"
 
+//styled components
+import styled from "styled-components"
+
 //us
 import hieu from "../devs/hieu"
 import isiah from "../devs/isiah"
@@ -25,17 +28,20 @@ let devs = [hieu, isiah, kyle, rich]
 //randomize the array
 let randomDevs = devs.sort(() => Math.random() - 0.5);
 
+const aboutDiv = styled.div`
+width: 100%;
+height: 100%;
+display: flex;
+flex-flow: column nowrap;
+justify-content: center;
+align-items: center;
+`
+
 export default function About() {
     // then map over the randomDevs array and display <Dev/>'s
-   return <div className="about">
+    return <aboutDiv>
         {randomDevs.map((dev, idx) => {
-            return (
-                <div className="dev-container" key={idx}>
-                    <Dev
-                        dev={dev}
-                    />
-                </div>
-            )
+                   return <Dev dev={dev} key={idx}/>
         })}
-    </div>
+    </aboutDiv>
 }
