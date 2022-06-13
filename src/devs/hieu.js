1c5bbcce6bb986dac345d32ae42b351433878d80
Hieu12319 <97079165+Hieu12319@users.noreply.github.com>
2022-06-12 20:47:41 -0700

    changes to hieu dev, still need photo

--- src/devs/hieu.js
+++ src/devs/hieu.js
@@ -2,7 +2,7 @@ export default {
     name: "Hieu Nguyen",
     img: "img.jpeg", //from public folder, we can dump images there
     github: "https://github.com/Hieu12319",
-    blurb: "I live to eat not eat to live."
+    blurb: "I live to eat not eat to live.",
     linkedin: "https://www.linkedin.com/in/hieu-nguyen-339286204/"
 }
     //perhaps add something else?
