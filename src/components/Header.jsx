18138577cb92782c1bae43864209d6dd1ae6a8d1
Richard Clarke <freshnewrich@gmail.com>
2022-06-12 17:29:16 -0700

    basic style Header, add image

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,30 @@
+//styled
+import styled from "styled-components"
 //import Link (for use to hit the List route and New route)
 import { Link } from "react-router-dom"
-import logo from "../Logo2.png"
+import logo from "../img/Logo2.png"
+import plus from "../img/plus1.png"
+
+const HeaderContainer = styled.div`
+display: flex;
+flex-flow: row nowrap;
+justify-content: space-between;
+align-items: center;
+padding: 0 1em;
+.add{
+    max-width: 2em;
+}
+.logo{
+    max-width:4em;
+}`
+
 export default function Header() {
-    return <div className="header">
-        
+    return <HeaderContainer>
         {/* //Image on left side with Link to list route */}
-        <Link to="/" className="logo"><img src={logo} alt="Pwnterest"/></Link>
+        <Link to="/" ><img src={logo} className="logo" alt="Pwnterest"/></Link>
         {/* // + on right side with Link to create route */}
-        <Link to="/create" className="createBtn"><button>Create Post</button></Link>
-    </div>
+        <Link to="/create" className="createBtn"><img className="add" src={plus} alt="New Post" /></Link>
+    </HeaderContainer>   
 }
 
 
