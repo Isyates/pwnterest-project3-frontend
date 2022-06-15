0592ce79ee967f4df53423728354c6c701a1cac2
Kyle Canamar <canamar.kyle@gmail.com>
2022-06-14 20:24:03 -0700

    update profile

--- src/devs/kyle.js
+++ src/devs/kyle.js
@@ -1,6 +1,8 @@
+import ProfileImage from "../img/Kyle.jpg"
+
 export default {
     name: "Kyle Canamar",
-    img: "https://media-exp2.licdn.com/dms/image/C5603AQErSHf_5d4rGA/profile-displayphoto-shrink_100_100/0/1651195536336?e=1660780800&v=beta&t=6YKt7gdpIWasF7raVagteH-1EgO_zoh2wWXJSmOoNfI", //from public folder, we can dump images there
+    img: ProfileImage, //from public folder, we can dump images there
     github: "https://github.com/kcanamar",
     blurb: "Currently I am a medicinal cannabis consultant for Kush 21, where I work with our guests to provide as much education on the products we carry.  Prior to that, I was privileged to work for a development restaurant company, where I was exposed to world class hospitality and leadership development. They showed me what I had been missing, leadership skills, trust building, and they helped reinspire my own development towards being my best self. What I am aspiring to grow into is a team member of a company who’s goals mirror my own in that I seek to provide a service to better the lives of others through collaboration and innovation. ",
     linkedin: "https://www.linkedin.com/in/kyle-canamar/"
