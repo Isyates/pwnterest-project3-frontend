7b697dfc56bcbe82b76faffa8aad888e4f4fec7f
Isiah Yates <isyates@outlook.com>
2022-06-14 21:17:44 -0700

    adds image to isiah dev profile

--- src/devs/isiah.js
+++ src/devs/isiah.js
@@ -1,6 +1,9 @@
+import image from "../img/isyates.jpg"
+
+
 export default {
     name: "Dev Name",
-    img: "img.jpeg", //from public folder, we can dump images there
+    img: `${image}`, //from public folder, we can dump images there
     github: "https://github.com/Isyates",
     blurb: "Dedicated to Gaming for all.",
     linkedin:"https://www.linkedin.com/in/isyates/"
