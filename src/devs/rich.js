f31932ca56a9af599e8e72bcf90028d298a06d03
Richard Clarke <freshnewrich@gmail.com>
2022-06-14 23:47:38 -0700

    update bio

--- src/devs/rich.js
+++ src/devs/rich.js
@@ -2,8 +2,7 @@ export default {
     name: "Rich Clarke",
     img: "https://raw.githubusercontent.com/richclarke0/richclarke0.github.io/main/img/me-sm.jpg", //from public folder, we can dump images there
     github: "https://github.com/richclarke0",
-    blurb: "I wish I was a little bit taller, I wish I was a baller, I wish I had a girl who looked good, I would call her. I wish I had a rabbit in a hat with a bat and a 6-4' Impala",
+    blurb: "Learning, solving problems, discovering new things, spending time outside of my comfort zone, not being afraid, not worrying about things, being wrong, being assertive, being ambitious, being empathetic, embracing change, uplifting others, adding value, working hard, having integrity, setting higher standards.",
     linkedin: "https://linkedin.com/in/rich-clarke-dev/"
-
     //perhaps add something else?
 }
