9c2857c7144be40584ddff4e600336f76682080d
Hieu12319 <97079165+Hieu12319@users.noreply.github.com>
2022-06-14 20:11:02 -0700

    updating about me

--- src/devs/hieu.js
+++ src/devs/hieu.js
@@ -1,6 +1,6 @@
 export default {
     name: "Hieu Nguyen",
-    img: "img.jpeg", //from public folder, we can dump images there
+    img: "https://media-exp2.licdn.com/dms/image/C5603AQE-EtdZ6bZ5lg/profile-displayphoto-shrink_400_400/0/1643041250254?e=1660780800&v=beta&t=NyxKjQVb58oJ65gStFuv2W2e4iB66z1dRZOeaFQU1zw", //from public folder, we can dump images there
     github: "https://github.com/Hieu12319",
     blurb: "I live to eat not eat to live.",
     linkedin: "https://www.linkedin.com/in/hieu-nguyen-339286204/"
